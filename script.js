73ff4051eab68ad6f258557e6bcaca92034e2360
Odogwu <becomingadev@gmail.com>
2022-10-16 17:07:44 +0100

    Getting closer on getting the search bar to work

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 'use strict';
 
-const searchField = document.querySelector('.search');
+let searchField = document.querySelector('.search');
+
+//Rendering the card function
+function renderCard(country) {
+  document.querySelector('.country-cards_container').innerHTML += `
+  <div class="card">
+    <div class='card-image'>
+      <img
+        src= ${country.flags.svg}
+        lt='${"The country's Flag"}'
+      />
+    </div>
+    <div class="card-content">
+      <h3 class="country-name">${country.name.common}</h3>
+      <p>Population: 
+        <span class="population">${country.population}</span>
+      </p>
+      <p>Region: 
+        <span class="region">${country.region}</span>
+      </p>
+      <p>Capital: 
+        <span class="capital">${country.capital}</span>
+      </p>
+    </div>
+  </div>
+`;
+}
 
 let allCountryName = [];
-let filterCard;
 //Fetching REST API and rendering the country cards
 fetch('https://restcountries.com/v3.1/all')
   .then(response => response.json())
   .then(countries => {
     countries.forEach(country => {
       allCountryName.push(country.name.common);
-      console.log(allCountryName);
 
-      filterCard = function () {
-        const searchFieldValue = searchField.value;
-        console.log(searchFieldValue);
-      };
-
-      document.querySelector('.country-cards_container').innerHTML += `
-        <div class="card">
-          <div class='card-image'>
-            <img
-              src= ${country.flags.svg}
-              lt='${"The country's Flag"}'
-            />
-          </div>
-          <div class="card-content">
-            <h3 class="country-name">${country.name.common}</h3>
-            <p>Population: 
-              <span class="population">${country.population}</span>
-            </p>
-            <p>Region: 
-              <span class="region">${country.region}</span>
-            </p>
-            <p>Capital: 
-              <span class="capital">${country.capital}</span>
-            </p>
-          </div>
-        </div>
-      `;
+      renderCard(country);
     });
-  });
 
-searchField.addEventListener('keyup', filterCard);
-console.log(allCountryName);
+    //Event Listerer for the search input
+    searchField.addEventListener('keyup', filterCard);
+  });
